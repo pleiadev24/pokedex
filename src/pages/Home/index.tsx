756bc806d99eeed842b7b15a3ddce670fd51f16e
vczb <viniciuszucatti@gmail.com>
2020-12-17 19:49:23 -0300

    fetch pokemon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,80 @@
+import { FormEvent, useState, useEffect } from 'react'
+import { Favorite, Search } from '@styled-icons/material-outlined'
+
 import * as S from './styles'
 
 import Logo from '../../components/Logo'
 import TextField from '../../components/TextField'
 import Button from '../../components/Button'
-const Home = () => (
-  <S.Wrapper>
-    <Logo />
-    <TextField placeholder="Digite o nome do pokemon" />
-    <Button>Procurar</Button>
-  </S.Wrapper>
-)
+
+import api from '../../services/api'
+import Heading from '../../components/Heading'
+
+type pokemonProps = {
+  name: string
+  image: imageProps
+  types: typeProps[]
+}
+
+type imageProps = {
+  front_default: string
+}
+
+type typeProps = {
+  type: {
+    name: string
+  }
+}
+
+const Home = () => {
+  const [name, setName] = useState('')
+  const [pokemon, setPokemon] = useState<pokemonProps>()
+
+  const fetchPokemon = async (name: string) => {
+    const result = await api.get(`pokemon/${name}`)
+    console.log(result)
+    console.log(result.data.name)
+
+    const poke = {
+      name: result.data.name,
+      image: result.data.sprites,
+      types: result.data.types
+    }
+    setPokemon(poke)
+  }
+
+  const handleSeach = async (Event: FormEvent) => {
+    Event.preventDefault()
+    fetchPokemon(name)
+  }
+
+  useEffect(() => {
+    console.log(pokemon)
+  }, [pokemon])
+  return (
+    <S.Wrapper>
+      <S.Form>
+        <Logo />
+        <TextField placeholder="Digite o nome do pokemon" onInput={setName} />
+        <Button onClick={(Event: FormEvent) => handleSeach(Event)}>
+          Buscar <Search size={20} />
+        </Button>
+        <Favorite size={30} aria-label="favorite" />
+      </S.Form>
+      {pokemon && (
+        <S.Card>
+          <img src={pokemon.image.front_default} />
+          <Heading lineLeft lineColor="secondary">
+            {pokemon.name}
+          </Heading>
+          {pokemon.types.map((item: typeProps, index) => (
+            <Heading key={index} lineLeft lineColor="secondary">
+              {item.type.name}
+            </Heading>
+          ))}
+        </S.Card>
+      )}
+    </S.Wrapper>
+  )
+}
 export default Home
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -6,8 +6,8 @@ export const Wrapper = styled(Container)`
   ${({ theme }) => css`
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
     min-height: 100vh;
+    align-items: center;
     > * {
       margin: 0 auto;
     }
@@ -16,5 +16,31 @@ export const Wrapper = styled(Container)`
         min-width: calc(${theme.grid.container} / 2);
       }
     `}
+  `}
+`
+
+export const Form = styled.form`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    > * {
+      margin: ${theme.spacings.medium};
+    }
+    img {
+      max-width: 13rem;
+    }
+    svg[aria-label='favorite'] {
+      fill: ${theme.colors.red};
+      cursor: pointer;
+    }
+  `}
+`
+
+export const Card = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    height: 100%;
+    background: ${theme.colors.primary};
+    padding: ${theme.spacings.medium};
   `}
 `
