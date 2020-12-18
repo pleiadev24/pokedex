f5026100fbe50f50a2297d56915de4d21b7f3f12
vczb <viniciuszucatti@gmail.com>
2020-12-18 19:44:01 -0300

    remove Home test

--- src/pages/Home/Home.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Home from './'
-import { renderWithTheme } from '../../utils/tests/helpers'
-
-test('render home correctly', () => {
-  const { container } = renderWithTheme(<Home />)
-  expect(container.firstChild).toBeInTheDocument()
-})
